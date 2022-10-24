0267b018300117846807817edf42e68b0ce94ef8
zhengyanwei <2020151022@email.szu.edu.cn>
2022-10-24 16:57:21 +0800

    changed at 20221024 16:51

--- 源.cpp
+++ 源.cpp
@@ -11,5 +11,11 @@ int main()
 	cout << "changed at 20221024 16:31:" << endl <<"please input two numbers:";
 	cin >> a >> b;
 	cout <<"the result of a plus b is " << a+b << endl;
+
+	//changed at 20221024 16:51
+	int a, b;
+	cout << "changed at 20221024 16:51:" << endl << "please input two numbers:";
+	cin >> a >> b;
+	cout << "the result of a multiplied by b is " << a * b << endl;
 	return 0;
 }
