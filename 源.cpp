155087f069e32a0a0c11b365a2d7a4454e71eddf
zhengyanwei <2020151022@email.szu.edu.cn>
2022-10-24 16:41:06 +0800

    changed at 20221024 16:31

--- 源.cpp
+++ 源.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
 using namespace std;
+
 int main()
 {
+	cout << "original file:" << endl;
 	cout << "Hello World!" << endl;
+	
+	//changed at 20221024 16:31
+	int a, b;
+	cout << "changed at 20221024 16:31:" << endl <<"please input two numbers:";
+	cin >> a >> b;
+	cout <<"the result of a plus b is " << a+b << endl;
 	return 0;
 }
